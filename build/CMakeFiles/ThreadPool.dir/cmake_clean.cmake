1c3930ad6ee17ceadd75f3191c2254fc20da7887
RyanDDDDDD <yainwrd@gmail.com>
2024-08-30 22:18:30 +1000

    fixed run method

---
+++ build/CMakeFiles/ThreadPool.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ThreadPool.dir/ThreadPool.cpp.obj"
+  "CMakeFiles/ThreadPool.dir/ThreadPool.cpp.obj.d"
+  "CMakeFiles/ThreadPool.dir/main.cpp.obj"
+  "CMakeFiles/ThreadPool.dir/main.cpp.obj.d"
+  "ThreadPool.exe"
+  "ThreadPool.exe.manifest"
+  "ThreadPool.lib"
+  "ThreadPool.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ThreadPool.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
