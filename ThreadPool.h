c3a271d0847a238b78cfaa5997585ffa8c697057
JoyDDDDNM <847714945@qq.com>
2024-09-01 23:20:54 +1000

    update Threadpoll header

--- ThreadPool.h
+++ ThreadPool.h
@@ -5,38 +5,39 @@
 #ifndef THREADPOOL_H
 #define THREADPOOL_H
 
-#include <vector>
-#include <thread>
-#include <future>
 #include <atomic>
+#include <functional>
+#include <future>
 #include <mutex>
 #include <queue>
-#include <functional>
+#include <thread>
+#include <vector>
 
 class ThreadPool {
 public:
-    ThreadPool(uint32_t num);
-    ~ThreadPool();
+    ThreadPool (uint32_t num);
+    ~ThreadPool ();
 
-    void start();
-    void stop();
-    int getThreadCount();
-    void addTask(std::function<void()> task);
-private:
-    ThreadPool(const ThreadPool& other) = delete;
-    ThreadPool& operator=(const ThreadPool& other) = delete;
+    void start ();
+    void stop ();
+    int getThreadCount ();
+    void addTask (std::function<void ()> task);
 
-    void run();
-    bool notStopped();
 private:
-    uint32_t                            _num;
-    std::queue<std::function<void()>>   _tasks;
-    std::vector<std::thread>            _threads;
-    std::mutex                          _mtx;
-    std::condition_variable             _cd;
-    std::atomic_bool                    _stop;
+    ThreadPool (const ThreadPool& other)            = delete;
+    ThreadPool& operator= (const ThreadPool& other) = delete;
+
+    void run ();
+    bool notStopped ();
+
+private:
+    const uint32_t _num;
+    std::queue<std::function<void ()>> _tasks;
+    std::vector<std::thread> _threads;
+    std::mutex _mtx;
+    std::condition_variable _cd;
+    std::atomic_bool _stop;
 };
 
 
-
-#endif //THREADPOOL_H
+#endif // THREADPOOL_H
